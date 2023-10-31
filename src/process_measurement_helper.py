23e2f49b5d68ef6d751fcc0989201e7149f7c919
up0pt <giftmath01au@gmail.com>
2023-10-31 18:23:16 +0900

    share session (stable)

--- src/process_measurement_helper.py
+++ src/process_measurement_helper.py
@@ -5,24 +5,39 @@ import functools
 import zenoh
 
 import ping_pong_measurer_zenoh_python as pzp
+from ping import Ping
 
 
-# ping pong using Zenoh-python in multiprocess
+# ping pong using Zenoh-python in multithread
+
+class PingPong():
+    def __init__(self, session, message):
+        self._session = session
+        self._message = message
+    
+    def start_ping_pong(self, node_id):
+        ping_node = Ping(node_id, self._session)
+        ping_node.start(self._message)
 
 if __name__ == "__main__":
     measurement_times = 10
     node_num = 1
     payload_bytes = 100
     message = 'a' * payload_bytes
+    session = zenoh.open()
 
+    start_pp = PingPong(session, message)
 
     for i in range(measurement_times):
-    # ProcessPoolExecutor の場合
+    # ThreadPoolExecutor の場合
         with concurrent.futures.ThreadPoolExecutor() as executor:
             # publish ping message concurrently
-            results = executor.map(functools.partial(pzp.start_ping_pong, message = message), list(range(node_num)))
-        
-        print(f">>>>>>>>>> #{i}/#{measurement_times}")
+            # results = executor.map(functools.partial(pzp.start_ping_pong, message = message), list(range(node_num)))
+            results = executor.map(start_pp.start_ping_pong, list(range(node_num)))
+
+        print(f">>>>>>>>>> #{i+1}/#{measurement_times}")
+
+    print("end ping loop")
 
     pzp.stop_ping_measurer()
     pzp.stop_ping_processes()
