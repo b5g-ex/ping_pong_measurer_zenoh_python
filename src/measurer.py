2f3d52332de157016753cb7568fc03595c1fe680
up0pt <giftmath01au@gmail.com>
2024-01-17 20:02:50 +0900

    fix type hint

--- src/measurer.py
+++ src/measurer.py
@@ -17,7 +17,7 @@ class State():
     node_id: int = 0
     ping_counts: int =  0
     start_time: int = 0 # in mili second
-    measure_time: list[list[int, int]] = field(default_factory=list) # [[start_time, end_time], [start_time, end_time], ...]
+    measure_time: list[list[int]] = field(default_factory=list) # [[start_time, end_time], [start_time, end_time], ...]
 
     # measurements: Measurement = Measurement() Stateを先に定義してからMeasurerを作るように変更
     
