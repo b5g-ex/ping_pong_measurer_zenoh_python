852c67481e445d769e4e585f2ba7f9c04fabf8e3
up0pt <giftmath01au@gmail.com>
2024-01-26 00:57:38 +0900

    add print for debug

--- src/ping.py
+++ src/ping.py
@@ -78,6 +78,7 @@ class PingManyToOneToOne():
 
     def callback(self, sample: Sample):
         if self._counter < self._node_num:
+            print(f"ping recv {self._node_id}")
             self._counter += 1
         else:
             self.queue.put("end")
@@ -85,6 +86,7 @@ class PingManyToOneToOne():
 
     def ping(self, message:str):
         self.publisher.put(message)
+        print(f"ping {self._node_id}")
 
     def start(self,message:str):
         self.ping(message)
